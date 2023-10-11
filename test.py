eec0e7138b8183586e440de1e28168854969058f
Ramdas Coudinya <69003379+RamdasCoundinya0716@users.noreply.github.com>
2023-10-11 15:48:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,43 +1,13 @@
 import pandas as pd
 from datetime import datetime
-
 from feast import FeatureStore
 from feast.data_source import PushMode
 
 def run_demo():
-    store = FeatureStore(repo_path="/home/shweta/ramdas/feast/my_project/feature_repo")
+    store = FeatureStore(repo_path=".")
     
-    # Define your entities and feature views
-    entities = [
-        # Replace with your actual entity definitions
-        {"name": "driver", "value_type": "int64", "event_timestamp_column": "event_timestamp"},
-    ]
-    
-    feature_views = [
-        # Replace with your actual feature view definitions
-        {
-            "name": "driver_hourly_stats",
-            "entities": ["driver"],
-            "features": [
-                {"name": "conv_rate", "value_type": "float"},
-                {"name": "acc_rate", "value_type": "float"},
-                {"name": "avg_daily_trips", "value_type": "float"},
-            ],
-            "batch_source": {...},  # Replace with your batch source config
-        },
-        {
-            "name": "transformed_conv_rate",
-            "entities": ["driver"],
-            "features": [
-                {"name": "conv_rate_plus_val1", "value_type": "float"},
-                {"name": "conv_rate_plus_val2", "value_type": "float"},
-            ],
-            "batch_source": {...},  # Replace with your batch source config
-        },
-    ]
-
     print("\n--- Run feast apply ---")
-    store.apply(feature_views=feature_views, entities=entities)
+    store.apply(repo_path="/home/shweta/ramdas/feast/my_project/feature_repo")
 
     print("\n--- Historical features for training ---")
     fetch_historical_features_entity_df(store, for_batch_scoring=False)
@@ -82,10 +52,63 @@ def run_demo():
     store.teardown()
 
 def fetch_historical_features_entity_df(store: FeatureStore, for_batch_scoring: bool):
-    # Your existing code for fetching historical features
+    entity_df = pd.DataFrame.from_dict(
+        {
+            "driver_id": [1001, 1002, 1003],
+            "event_timestamp": [
+                datetime(2021, 4, 12, 10, 59, 42),
+                datetime(2021, 4, 12, 8, 12, 10),
+                datetime(2021, 4, 12, 16, 40, 26),
+            ],
+            "label_driver_reported_satisfaction": [1, 5, 3],
+            "val_to_add": [1, 2, 3],
+            "val_to_add_2": [10, 20, 30],
+        }
+    )
+    if for_batch_scoring:
+        entity_df["event_timestamp"] = pd.to_datetime("now", utc=True)
+
+    training_df = store.get_historical_features(
+        entity_df=entity_df,
+        features=[
+            "driver_hourly_stats:conv_rate",
+            "driver_hourly_stats:acc_rate",
+            "driver_hourly_stats:avg_daily_trips",
+            "transformed_conv_rate:conv_rate_plus_val1",
+            "transformed_conv_rate:conv_rate_plus_val2",
+        ],
+    ).to_df()
+    print(training_df.head())
 
 def fetch_online_features(store, source: str = ""):
-    # Your existing code for fetching online features
+    entity_rows = [
+        {
+            "driver_id": 1001,
+            "val_to_add": 1000,
+            "val_to_add_2": 2000,
+        },
+        {
+            "driver_id": 1002,
+            "val_to_add": 1001,
+            "val_to_add_2": 2002,
+        },
+    ]
+    if source == "feature_service":
+        features_to_fetch = store.get_feature_service("driver_activity_v1")
+    elif source == "push":
+        features_to_fetch = store.get_feature_service("driver_activity_v3")
+    else:
+        features_to_fetch = [
+            "driver_hourly_stats:acc_rate",
+            "transformed_conv_rate:conv_rate_plus_val1",
+            "transformed_conv_rate:conv_rate_plus_val2",
+        ]
+    returned_features = store.get_online_features(
+        features=features_to_fetch,
+        entity_rows=entity_rows,
+    ).to_dict()
+    for key, value in sorted(returned_features.items()):
+        print(key, " : ", value)
 
 if __name__ == "__main__":
     run_demo()
