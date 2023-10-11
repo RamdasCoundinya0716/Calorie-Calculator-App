6e4295dc2738061bf0772f7369059a3b3c9a2ca1
Ramdas Coudinya <69003379+RamdasCoundinya0716@users.noreply.github.com>
2023-10-11 15:58:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 fs = FeatureStore(repo_path="/home/shweta/ramdas/feast/my_project/feature_repo")
 driver = Entity(name="driver_id", description="driver id")
 driver_hourly_stats = FileSource(
-    path="project/feature_repo/data/driver_stats.parquet",
+    path="/home/shweta/ramdas/feast/my_project/feature_repo/driver_stats.parquet",
     timestamp_field="event_timestamp",
     created_timestamp_column="created",
 )
