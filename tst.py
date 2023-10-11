a0ced6a997a2454670b8dc22d0dfabb9722cf8af
Ramdas Coudinya <69003379+RamdasCoundinya0716@users.noreply.github.com>
2023-10-11 15:15:45 +0530

    Update tst.py

--- tst.py
+++ tst.py
@@ -6,29 +6,22 @@ from feast.data_source import PushMode
 
 def run_demo():
   store = FeatureStore(repo_path=".")
-
-  # Apply the feature definitions.
   store.apply()
-
-  # Retrieve historical features for training.
   fetch_historical_features_entity_df(store, for_batch_scoring=False)
-
-  # Retrieve historical features for batch scoring.
   fetch_historical_features_entity_df(store, for_batch_scoring=True)
 
-  # Load features into online store.
   store.materialize_incremental(end_date=datetime.now())
 
-  # Retrieve online features.
+
   fetch_online_features(store)
 
-  # Retrieve online features through a feature service.
+
   fetch_online_features(store, source="feature_service")
 
-  # Retrieve online features retrieved (using feature service v3, which uses a feature view with a push source)
+
   fetch_online_features(store, source="push")
 
-  # Simulate a stream event ingestion of the hourly stats df.
+
   event_df = pd.DataFrame.from_dict(
     {
       "driver_id": [1001],
@@ -46,7 +39,7 @@ def run_demo():
   print(event_df)
   store.push("driver_stats_push_source", event_df, to=PushMode.ONLINE_AND_OFFLINE)
 
-  # Retrieve online features again with updated values from a stream push.
+
   fetch_online_features(store, source="push")
 
 if __name__ == '__main__':
